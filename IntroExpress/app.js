b620e7fb1ce59e8b1b25a8770f7f7f0a9c5a2f90
Corey Greenwald <coreygreen1108@gmail.com>
2019-01-04 16:40:16 -0500

    add notes for intro express lecture

--- IntroExpress/app.js
+++ IntroExpress/app.js
@@ -3,14 +3,25 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan');
 
-const puppies = require('./puppies');
-
+const puppies = require('./api/puppies'); 
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({
     extended: false
 }));
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/puppies', puppies)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+//You would put yuor error handling middleware at the end!
+// Check out the docs.
+// https://expressjs.com/en/guide/error-handling.html
+
 // const coreysLoggingMiddleware = () => {
 //     return function(req, res, next){
 //         console.log(req.method, req.path);
@@ -19,35 +30,8 @@ app.use(express.json())
 // }
 // app.use(coreysLoggingMiddleware())
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
-
-app.get('/puppies', (req, res) => {
-    // res.status(200)
-    // res.send('<h1>This is soooo cool!</h1>')
-    res.send(puppies);
-})
-
-// app.put('/puppies/:id', (req, res) => {
-
-// })
 
 //param called id is accessible req.params.id
-app.get('/puppies/:id', (req, res) => {
-    const puppyId = Number(req.params.id) - 1;
-    res.send(puppies[puppyId]);
-})
-
-app.post('/puppies', (req, res) => {
-    const age = Number(req.body.age) || 0;
-    const name = req.body.name || 'Tido';  
-    puppies.push({
-        name,
-        age
-    })
-    res.send(puppies);
-})
 
 
 app.listen(3000);
