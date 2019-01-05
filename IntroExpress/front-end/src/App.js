bbaebc4fe0647244d1f2d0b7a4fac13a95ad3ab4
Geoff Bass <geoff.c.bass@gmail.com>
2019-01-05 11:31:29 -0500

    adds form to add a new puppy

--- IntroExpress/front-end/src/App.js
+++ IntroExpress/front-end/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
@@ -9,6 +8,7 @@ class App extends Component {
     this.state = {
       puppies: []
     }
+    this.addPuppy = this.addPuppy.bind(this);
   }
 
   async componentDidMount() {
@@ -19,15 +19,70 @@ class App extends Component {
     });
   }
 
+  addPuppy(newPuppies) {
+    this.setState({
+      puppies: newPuppies
+    })
+  }
+
   render() {
-    console.log('puppies', this.state.puppies);
     return (
       <div>
         <h1>Hello Impact Fellowship</h1>
-        <h3>Puppies will go here:</h3>
+        <NewPuppyForm addPuppy={this.addPuppy} />
+        <h3>Puppies:</h3>
         {
           this.state.puppies.map(puppy => <p>{puppy.name}, {puppy.age} year{puppy.age > 1 ? 's' : null} old</p>)
         }
+      </div>
+    );
+  }
+}
+
+class NewPuppyForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      age: ''
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAgeChange = this.handleAgeChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleAgeChange (changeEvent) {
+    this.setState({
+      age: changeEvent.target.value
+    })
+  }
+
+  handleNameChange (changeEvent) {
+    this.setState({
+      name: changeEvent.target.value
+    })
+  }
+
+  async handleSubmit (submitEvent) {
+    submitEvent.preventDefault();
+    const response = await axios.post('/puppies', this.state);
+    const puppies = response.data;
+    this.props.addPuppy(puppies);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Add a new puppy!</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name:</label>
+          <input onChange={this.handleNameChange} type="text" name="name" />
+          <br/>
+          <label>Age:</label>
+          <input onChange={this.handleAgeChange} type="text" name="age" />
+          <br/>
+          <button type="submit">Add Puppy</button>
+        </form>
       </div>
     );
   }
